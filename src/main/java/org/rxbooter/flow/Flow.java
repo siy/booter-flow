35206f6c939322819c07cdcd925cda5a9e5a4c86
Sergiy Yevtushenko <sergiy.yevtushenko@grandparade.co.uk>
2017-06-27 15:48:33 +0200

    Intermediate commit. Updated approach to builders

--- src/main/java/org/rxbooter/flow/Flow.java
+++ src/main/java/org/rxbooter/flow/Flow.java
@@ -10,7 +10,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-import static org.rxbooter.flow.FlowBuilders.*;
+//import static org.rxbooter.flow.FlowBuilders.*;
 import static org.rxbooter.flow.Tuples.*;
 
 public class Flow<O extends Tuple, I extends Tuple> {
@@ -82,40 +82,40 @@ public class Flow<O extends Tuple, I extends Tuple> {
         return new Flow<>(flow1, flow2);
     }
 
-    @SuppressWarnings("unchecked")
-    public static <O extends Tuple, I extends Tuple> Flow <O, I> of(FlowBuilder<O> builder) {
-        List<Step<? extends Tuple, ? extends Tuple>> steps = new ArrayList<>();
-        builder.apply((step) -> steps.add((Step<? extends Tuple, ? extends Tuple>) step));
-        return new Flow<>(steps);
-    }
-
-    public static<T1> FlowBuilder1<Tuple1<T1>, T1> take(Class<T1> param1) {
-        return new FlowBuilder1<>(null);
-    }
-
-    public static<T1, T2> FlowBuilder2<Tuple2<T1, T2>, T1, T2> take(Class<T1> param1, Class<T2> param2) {
-        return new FlowBuilder2<>(null);
-    }
-
-    public static<T1, T2, T3> FlowBuilder3<Tuple3<T1, T2, T3>, T1, T2, T3> take(Class<T1> param1, Class<T2> param2, Class<T3> param3) {
-        return new FlowBuilder3<>(null);
-    }
-
-    public static<T1, T2, T3, T4> FlowBuilder4<Tuple4<T1, T2, T3, T4>, T1, T2, T3, T4> take(Class<T1> param1, Class<T2> param2, Class<T3> param3, Class<T4> param4) {
-        return new FlowBuilder4<>(null);
-    }
-
-    public static<T1, T2, T3, T4, T5> FlowBuilder5<Tuple5<T1, T2, T3, T4, T5>, T1, T2, T3, T4, T5> take(Class<T1> param1, Class<T2> param2, Class<T3> param3, Class<T4> param4, Class<T5> param5) {
-        return new FlowBuilder5<>(null);
-    }
-
-    public static<T1, T2, T3, T4, T5, T6> FlowBuilder6<Tuple6<T1, T2, T3, T4, T5, T6>, T1, T2, T3, T4, T5, T6> take(Class<T1> param1, Class<T2> param2, Class<T3> param3, Class<T4> param4, Class<T5> param5, Class<T6> param6) {
-        return new FlowBuilder6<>(null);
-    }
-
-    public static<T1, T2, T3, T4, T5, T6, T7> FlowBuilder7<Tuple7<T1, T2, T3, T4, T5, T6, T7>, T1, T2, T3, T4, T5, T6, T7> take(Class<T1> param1, Class<T2> param2, Class<T3> param3, Class<T4> param4, Class<T5> param5, Class<T6> param6, Class<T7> param7) {
-        return new FlowBuilder7<>(null);
-    }
+//    @SuppressWarnings("unchecked")
+//    public static <O extends Tuple, I extends Tuple> Flow <O, I> of(FlowBuilder<O> builder) {
+//        List<Step<? extends Tuple, ? extends Tuple>> steps = new ArrayList<>();
+//        builder.apply((step) -> steps.add((Step<? extends Tuple, ? extends Tuple>) step));
+//        return new Flow<>(steps);
+//    }
+//
+//    public static<T1> FlowBuilder1<Tuple1<T1>, T1> take(Class<T1> param1) {
+//        return new FlowBuilder1<>(null);
+//    }
+//
+//    public static<T1, T2> FlowBuilder2<Tuple2<T1, T2>, T1, T2> take(Class<T1> param1, Class<T2> param2) {
+//        return new FlowBuilder2<>(null);
+//    }
+//
+//    public static<T1, T2, T3> FlowBuilder3<Tuple3<T1, T2, T3>, T1, T2, T3> take(Class<T1> param1, Class<T2> param2, Class<T3> param3) {
+//        return new FlowBuilder3<>(null);
+//    }
+//
+//    public static<T1, T2, T3, T4> FlowBuilder4<Tuple4<T1, T2, T3, T4>, T1, T2, T3, T4> take(Class<T1> param1, Class<T2> param2, Class<T3> param3, Class<T4> param4) {
+//        return new FlowBuilder4<>(null);
+//    }
+//
+//    public static<T1, T2, T3, T4, T5> FlowBuilder5<Tuple5<T1, T2, T3, T4, T5>, T1, T2, T3, T4, T5> take(Class<T1> param1, Class<T2> param2, Class<T3> param3, Class<T4> param4, Class<T5> param5) {
+//        return new FlowBuilder5<>(null);
+//    }
+//
+//    public static<T1, T2, T3, T4, T5, T6> FlowBuilder6<Tuple6<T1, T2, T3, T4, T5, T6>, T1, T2, T3, T4, T5, T6> take(Class<T1> param1, Class<T2> param2, Class<T3> param3, Class<T4> param4, Class<T5> param5, Class<T6> param6) {
+//        return new FlowBuilder6<>(null);
+//    }
+//
+//    public static<T1, T2, T3, T4, T5, T6, T7> FlowBuilder7<Tuple7<T1, T2, T3, T4, T5, T6, T7>, T1, T2, T3, T4, T5, T6, T7> take(Class<T1> param1, Class<T2> param2, Class<T3> param3, Class<T4> param4, Class<T5> param5, Class<T6> param6, Class<T7> param7) {
+//        return new FlowBuilder7<>(null);
+//    }
 
     public static class ExecutableFlow<O extends Tuple, I extends Tuple> {
         private final List<Step<? extends Tuple, ? extends Tuple>> steps;
--- src/test/java/org/rxbooter/flow/PipelineTest.java
+++ src/test/java/org/rxbooter/flow/PipelineTest.java
@@ -10,11 +10,11 @@ public class PipelineTest {
 
     @Test
     public void shouldCompileAndRunSimpleFlow() throws Exception {
-        Flow<Tuple1<String>, Tuple1<String>> flow = take(String.class)
-                                                                   .apply2((s) -> of(s, s.toLowerCase()))
-                                                                   .apply1((s1, s2) -> with(s1 + " " + s2))
-                                                                   .build();
-
+//        Flow<Tuple1<String>, Tuple1<String>> flow = take(String.class)
+//                                                                   .apply2((s) -> of(s, s.toLowerCase()))
+//                                                                   .apply1((s1, s2) -> with(s1 + " " + s2))
+//                                                                   .build();
+//
         //TODO: fix it
         //assertThat(Reactor.waitFor(flow.cursor(of("abcABC"))).get(0)).isEqualTo("abcABC abcabc");
     }
